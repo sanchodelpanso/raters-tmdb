1685667711b6bc9bc37fb63458c0218ac69ac2c2
Salivonenko, Oleksandr <Oleksandr.Salivonenko@vimn.com>
2018-08-02 23:34:17 +0200

    force update every first week of mounth

--- src/app.schedule.ts
+++ src/app.schedule.ts
@@ -3,22 +3,25 @@ import { tmdbWorker } from './workers/tmdb.worker';
 import { searchApi } from './services/search-api-service';
 import { state } from './state/app.state';
 import { State } from './state/state';
+import * as moment from 'moment'
 
 export class AppSchedule {
 
     public runMovieUpdates() {
         const rule = '0 0 14 * * 1'; //Every Monday at 14.00 UTC
 
-        scheduleJob(rule, () => {
+        scheduleJob(rule, fireDate => {
             let updatedNumber: any = {
                 movies: null,
                 shows: null
             };
 
-            tmdbWorker.updateTvSeries()
+            const force = Math.ceil(fireDate.getDate() / 7) === 1;
+
+            tmdbWorker.updateTvSeries(force)
                 .then(updated =>  {
                     updatedNumber.shows = updated;
-                    return tmdbWorker.updateMovies();
+                    return tmdbWorker.updateMovies(force);
                 })
                 .then(updated => {
                     updatedNumber.movies = updated;
